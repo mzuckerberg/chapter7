9c674cca1a5e96d6bc88a48e7a01db8eaad5d6f2
Mark Gainan <Mooks@Marks-MBP.cable.rcn.com>
2019-02-07 19:45:07 -0500

    added step 3 find matches within clipboard text

--- phoneNumberProj.py
+++ phoneNumberProj.py
@@ -23,6 +23,8 @@ display some kind of message if no matches found
 #! python3
 import pyperclip, re
 
+# phone number regex
+
 phoneRegex = re.compile(r'''(
     (\d{3}|\(\d{3}\))?              # area code optional
     (s|-|\.)?                       # separator optional
@@ -30,10 +32,10 @@ phoneRegex = re.compile(r'''(
     (s|-|\.)                        # separator non optional
     (\d{4})                         # last 4 digits
     (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
+    '''), re.VERBOSE)
 
-'''), re.VERBOSE)
+# email regex
 
-# TODO: Create email regex
 emailRegex = re.compile(r'''(
     [a-zA-Z0-9._%+-]+       # username
     @                       # at symbol
@@ -42,5 +44,14 @@ emailRegex = re.compile(r'''(
     )''', re.VERBOSE)
 
 # TODO: find matches in clipboard text
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
 
 # TODO: copy results to the clipboard
